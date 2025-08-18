a49c12f470925b3673fc39ec09ab459ed364e8cf
Mortimer, Nicolas (Environment, IOMRC Crawley) <nick.mortimer@csiro.au>
2025-08-18 13:53:06 +0800

    Ok conda activation handle correctly

--- {{cookiecutter.field_trip_id}}/bin/{{cookiecutter.field_trip_id}}_register_cards.bat
+++ {{cookiecutter.field_trip_id}}/bin/{{cookiecutter.field_trip_id}}_register_cards.bat
@@ -3,12 +3,24 @@ SET SCRIPT_DIR=%~dp0
 CD /D "%SCRIPT_DIR%.."
 
 REM Initialize conda for batch files and activate base environment
-call "%LOCALAPPDATA%\anaconda3\Scripts\activate.bat" base || call "%LOCALAPPDATA%\miniconda3\Scripts\activate.bat" base || call "%USERPROFILE%\anaconda3\Scripts\activate.bat" base || call "%USERPROFILE%\miniconda3\Scripts\activate.bat" base || (
-    echo ERROR: Could not find Anaconda/Miniconda installation
-    echo Please ensure Anaconda or Miniconda is installed
-    pause
-    exit /b 1
-)
+call "%LOCALAPPDATA%\anaconda3\Scripts\activate.bat" base
+if not errorlevel 1 goto conda_activated
+
+call "%LOCALAPPDATA%\miniconda3\Scripts\activate.bat" base
+if not errorlevel 1 goto conda_activated
+
+call "%USERPROFILE%\anaconda3\Scripts\activate.bat" base
+if not errorlevel 1 goto conda_activated
+
+call "%USERPROFILE%\miniconda3\Scripts\activate.bat" base
+if not errorlevel 1 goto conda_activated
+
+echo ERROR: Could not find Anaconda/Miniconda installation
+echo Please ensure Anaconda or Miniconda is installed
+pause
+exit /b 1
+
+:conda_activated
 
 REM Activate the cardmanager environment
 call conda activate cardmanager
